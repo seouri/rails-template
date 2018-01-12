c05d62693ae4ad522f5c41be514b14bd5aa34d07
Joon Lee <seouri@gmail.com>
2018-01-12 13:46:08 -0500

    Update template.rb

--- template.rb
+++ template.rb
@@ -34,19 +34,19 @@ if defined?(PryRails::RAILS_PROMPT)
 end
 CODE
 
-# # bullet gem
-# environment nil, env: "development" do
-# <<-CODE
-# config.after_initialize do
-#     Bullet.enable = true
-#     Bullet.alert = true
-#     Bullet.bullet_logger = true
-#     Bullet.console = true
-#     Bullet.rails_logger = true
-#     Bullet.add_footer = true
-#   end
-# CODE
-# end
+# bullet gem
+environment nil, env: "development" do
+<<-CODE
+config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.rails_logger = true
+  Bullet.add_footer = true
+end
+CODE
+end
 
 # dotenv-rails gem
 file ".env", ""

508bd0e698c7eb242123e8d60be3c8a1f43f10f2
Joon Lee <seouri@gmail.com>
2018-01-19 09:17:09 -0500

    Update template.rb

--- template.rb
+++ template.rb
@@ -83,6 +83,7 @@ environment "config.eager_load = false",
 file ".ruby-gemset", "#{app_name}"
 file ".ruby-version", "2.5.0"
 run "rvm gemset create #{app_name}"
+run "rvm gemset use #{app_name}"
 
 after_bundle do
   rails_command("db:create")

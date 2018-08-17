989ad5f28803d5d0d2c603165fc8e2c812a2308d
Joon Lee <seouri@gmail.com>
2018-08-17 14:23:13 -0400

    Update template.rb

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # rails-template
 
 ```
-rails new cool_app -d postgresql -m https://raw.githubusercontent.com/seouri/rails-template/master/template.rb
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install openssl cmake postgres
+curl -sSL https://get.rvm.io | bash -s stable
+rvm install 2.5.1 --default --with-openssl-dir=`brew --prefix openssl`
+gem install rails
+rails new cool_app --skip-coffee -f -B -d postgresql -m https://raw.githubusercontent.com/seouri/rails-template/master/template.rb
+cd cool_app
+gem update bundler
+gem install nokogiri -- --use-system-libraries=true --with-xml2-include=`xcrun --show-sdk-path`/usr/include/libxml2
+bundle install
 ```
--- template.rb
+++ template.rb
@@ -2,10 +2,7 @@ git :init
 git add: "."
 git commit: "-a -m 'Initial commit'"
 
-run "bundle config build.nokogiri --use-system-libraries"
-
 gem_group :development, :test do
-  gem "pronto"
   gem "pronto-brakeman", require: false
   gem "pronto-fasterer", require: false
   gem "pronto-flay", require: false
@@ -13,6 +10,7 @@ gem_group :development, :test do
   gem "pronto-reek", require: false
   gem "pronto-rubocop", require: false
   gem "pronto-simplecov", require: false
+  gem "pronto", require: false
   gem "pry-rails"
 end
 
@@ -21,13 +19,15 @@ gem_group :development do
   gem "bundler-audit", require: false
   gem "dotenv-rails"
   gem "letter_opener_web"
-  gem "rubocop-rails", require: false
   gem "ruby_audit", require: false
 end
 
 gem_group :test do
+  gem "minitest-ci"
   gem "simplecov", require: false
 end
+
+gem "ar-uuid"
 
 # pry-rails gem
 file ".pryrc", <<-CODE
@@ -56,21 +56,42 @@ file ".env.development", ""
 file ".env.local", ""
 run "echo '.env.local' >> .gitignore"
 
+# minitest-ci gem
+run "echo '/test/reports' >> .gitignore"
+
 # letter_opener_web gem
 environment "config.action_mailer.delivery_method = :letter_opener_web",
             env: "development"
 
 route <<-CODE
-if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/sent_emails"
-end
+mount LetterOpenerWeb::Engine, at: '/sent_emails' if Rails.env.development?
 CODE
 
-# rubocop-rails gem 
 file ".rubocop.yml", <<-CODE
-  inherit_gem:
-    rubocop-rails:
-      - config/rails.yml
+Metrics/BlockLength:
+  Exclude:
+    - 'config/environments/development.rb'
+
+Metrics/LineLength:
+  Max: 130
+
+Style/ClassAndModuleChildren:
+  Exclude:
+    - 'test/test_helper.rb'
+
+Style/Documentation:
+  Exclude:
+    - 'spec/**/*'
+    - 'test/**/*'
+    - 'app/helpers/application_helper.rb'
+    - 'app/mailers/application_mailer.rb'
+    - 'app/models/application_record.rb'
+    - 'config/application.rb'
+
+Style/MixinUsage:
+  Exclude:
+    - 'bin/setup'
+    - 'bin/update'
 CODE
 
 # simplecov gem
@@ -82,14 +103,137 @@ environment "config.eager_load = false",
 
 # rvm
 file ".ruby-gemset", "#{app_name}"
-file ".ruby-version", "2.5.1"
 run "rvm gemset create #{app_name}"
-run "rvm use 2.5.1@#{app_name}"
+
+# CircleCI
+file ".circleci/config.yml", <<-CODE
+version: 2
+jobs:
+  build:
+    parallelism: 1
+
+    docker:
+      - image: circleci/ruby:2.5.1-node-browsers
+      - image: circleci/postgres:9.6.9-alpine-ram
+
+    environment:
+      RAILS_ENV: "test"
+      PGHOST: "localhost"
+      PGUSER: "postgres"
+
+    steps:
+
+      - checkout
+
+      - restore_cache:
+          keys:
+          - gem-cache-{{ .Branch }}-{{ checksum "Gemfile.lock" }}
+          - gem-cache-{{ checksum "Gemfile.lock" }}
+          - gem-cache
+
+      # For rugged gem (pronto gem dependency)
+      - run: sudo apt-get install cmake
+
+      - run:
+          name: Bundle Install
+          command: bundle check --path=vendor/bundle || bundle install --path=vendor/bundle --jobs=4 --retry=3
+
+      - run:
+          name: Ruby Audit
+          command: bundle exec ruby-audit check
+
+      - run:
+          name: Bundle Audit
+          command: bundle exec bundle-audit check --update
+
+      - run:
+          name: Brakeman
+          command: bundle exec brakeman
+
+      - run:
+          name: Rubocop
+          command: bundle exec rubocop
+
+      # Store bundle cache
+      - save_cache:
+          key: gem-cache-{{ .Branch }}-{{ checksum "Gemfile.lock" }}
+          paths:
+            - vendor/bundle
+
+      - save_cache:
+          key: gem-cache-{{ checksum "Gemfile.lock" }}
+          paths:
+            - vendor/bundle
+
+      - save_cache:
+          key: gem-cache
+          paths:
+            - vendor/bundle
+
+      - run:
+          name: Set up DB
+          command: bundle exec rails db:setup
+
+      - run:
+          name: Rails Test
+          command: bundle exec rake test
+
+      - store_test_results:
+          path: test/reports
+          destination: test-results/rails
+
+      - store_artifacts:
+          path: test/reports
+          destination: test-results/MiniTest
+
+      - store_artifacts:
+          path: coverage
+          destination: coverage
+CODE
+
+# README
+file "README.md", <<-CODE
+## Getting Started
+
+1. Install [Homebrew](https://brew.sh/):
+
+    ```sh
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ```
+
+2. Install OpenSSL libraries:
+
+    ```sh
+    brew install openssl
+    ```
+
+3. Install RVM and Ruby:
+
+    ```sh
+    curl -sSL https://get.rvm.io | bash -s stable
+    # In a new session (restart your terminal or open new tab):
+    rvm install 2.5.1 --default --with-openssl-dir=`brew --prefix openssl`
+    ```
+
+4. Set up RVM gemset:
+
+    ```sh
+    rvm gemset create #{app_name}
+    rvm use 2.5.1@#{app_name}
+    gem update --system
+    gem update bundler
+    gem install nokogiri -- --use-system-libraries=true --with-xml2-include=`xcrun --show-sdk-path`/usr/include/libxml2
+    ```
+CODE
 
 after_bundle do
+  run "rvm gemset use #{app_name}"
+  run "gem update bundler"
+  run "gem install nokogiri -- --use-system-libraries=true --with-xml2-include=`xcrun --show-sdk-path`/usr/include/libxml2"
+  run "bundle install"
   rails_command("db:create")
   rails_command("db:migrate")
   run "rubocop -a"
   git add: "."
-  git commit: "-a -m 'Set up development gems'"
+  git commit: "-a -m 'Set up development environment'"
 end
